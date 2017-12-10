2f26ec7e4ab4e6c2ec57c5b81f907b07796e5916
Krysol <Krysol11111@gmail.com>
2017-12-10 13:39:24 +0100

    added QuestionCard component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,26 +1,47 @@
 <template>
   <div>
-    <h3>{{this.questions[0].question}}</h3>
-    <ol>
-      <li v-for="item in items"></li>
-    </ol>
+    <QuestionCard
+      :question="currentQuestion"
+      :onAnswer="checkAnswer"
+    />
   </div>
 </template>
 
 <script>
   import data from "../../db"
+  import QuestionCard from "./QuestionCard"
   export default {
     name: 'Game',
+    components:{
+    	QuestionCard,
+    },
     data () {
       return {
+      	current: 0,
         questions: data.questions,
       }
     },
     computed:{
-      isInvalid: function(){
-        return this.message.length <= 3
-      }
+      answers: function(){
+      	const answers = [
+          ...this.currentQuestion.incorrect_answers,
+          this.currentQuestion.correct_answer,
+        ];
+        return answers;
+      },
+      currentQuestion: function(){
+      	return this.questions[this.current];
+      },
     },
+    methods:{
+    	checkAnswer: function (answer)
+      {
+        const isAnswerCorrect = answer === this.currentQuestion.correct_answer;
+        if (isAnswerCorrect){
+        	this.current++;
+        }
+      }
+    }
   }
 </script>
 
---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>{{this.question.question}}</h3>
+    <ol>
+      <li v-for="answer in answers" @click="onAnswer(answer)">{{answer}}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default{
+  	name: "QuestionCard",
+    props: {
+  		question: {
+  			default: () => ({}),
+      },
+      onAnswer: {
+  			default: ()=>{}
+  	  },
+    },
+    computed: {
+      answers: function ()
+      {
+        const answers = [
+          ...this.question.incorrect_answers,
+          this.question.correct_answer,
+        ];
+        return answers;
+      },
+    }
+  }
+</script>
+
+<style>
+
+</style>
