4906cf20bcd06bd088bb0b4e1979ff71a251d071
Krysol <Krysol11111@gmail.com>
2017-12-10 12:17:30 +0100

    added data

---
+++ src/components/Game.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>{{this.questions[0].question}}</h3>
+    <ol>
+      <li v-for="item in items"></li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  import data from "../../db"
+  export default {
+    name: 'Game',
+    data () {
+      return {
+        questions: data.questions,
+      }
+    },
+    computed:{
+      isInvalid: function(){
+        return this.message.length <= 3
+      }
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
